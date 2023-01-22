16bb8a9c621104e6d1440f485288879e806f4afb
Konrad Fedorczyk <fedek6@gmail.com>
2023-01-22 12:16:16 +0100

    Add new features

--- nanodoro.app.js
+++ nanodoro.app.js
@@ -1,21 +1,59 @@
-const INTERVAL = 1500;
-let counterInterval, currentTime, paused;
+const LONG_INTERVAL = 1500;
+const SHORT_INTERVAL = 300;
+const ALARM_REPEAT = 3;
+let isCurrentIntervalLong = true;
+let counterInterval;
+let currentTime;
+let paused;
+let count = 0;
+
+function fancyTimeFormat(duration) {
+  var hrs = ~~(duration / 3600);
+  var mins = ~~((duration % 3600) / 60);
+  var secs = ~~duration % 60;
+  var ret = "";
+
+  if (hrs > 0) {
+    ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+  }
+
+  ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+  ret += "" + secs;
+  return ret;
+}
 
 function drawLabels() {
-  g.setFontAlign(-1, 0);
-  g.setFont("6x8", 7);
   g.setFontAlign(0, 0, 3);
   g.setFont("6x8", 1);
   g.drawString("Reset", 230, 30);
   g.drawString("Pause", 230, g.getHeight() / 2);
 }
 
+function drawState() {
+  g.setFont("6x8", 2);
+  g.setFontAlign(0, 0);
+  g.drawString(isCurrentIntervalLong ? "LONG" : "SHORT", 120, 75);
+
+  g.setFont("6x8", 4);
+  g.drawString(count, 50, 20);
+}
+
+function drawTime() {
+  const now = new Date();
+  const hoursAndMinutes = now.getHours() + ":" + now.getMinutes();
+  g.setFont("6x8", 2);
+  g.setFontAlign(0, 0, 0);
+  g.drawString(hoursAndMinutes, 20, 200);
+}
+
 function draw() {
   g.clear();
   g.setFontAlign(0, 0);
   g.setFont("6x8", 7);
-  g.drawString(currentTime, 120, 120);
+  g.drawString(fancyTimeFormat(currentTime), 120, 120);
   drawLabels();
+  drawState();
+  drawTime();
 }
 
 function pauseHandler() {
@@ -26,7 +64,8 @@ function pauseHandler() {
 }
 
 function init() {
-  currentTime = INTERVAL;
+  currentTime = isCurrentIntervalLong ? LONG_INTERVAL : SHORT_INTERVAL;
+
   draw();
   drawLabels();
   paused = false;
@@ -38,21 +77,29 @@ function init() {
   }, BTN1);
 }
 
-const wait = () => new Promise(resolve => {
-  setTimeout(() => {
-    resolve(true);
-  }, 500);
-});
+const wait = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, 500);
+  });
+};
 
 function alarm() {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     Bangle.buzz(1000, 1)
       .then(wait)
       .then(() => Bangle.buzz(1000, 0.5))
       .then(wait)
       .then(resolve);
   });
+}
 
+function repeatPromise(func, times) {
+  let promise = Promise.resolve();
+  let repeat = times;
+  while (repeat-- > 0) promise = promise.then(func);
+  return promise;
 }
 
 function mainLoop() {
@@ -63,14 +110,14 @@ function mainLoop() {
     draw();
 
     if (currentTime === 0) {
-      clearInterval(counterInterval);
-      alarm().then(alarm).then(alarm).then(alarm);
+      if (isCurrentIntervalLong) {
+        count = count + 1;
+      }
 
-      E.showMessage("Congratulations! You've finished your tomato.", {
-        img: atob(
-          "FBQBAfgAf+Af/4P//D+fx/n+f5/v+f//n//5//+f//n////3//5/n+P//D//wf/4B/4AH4A="
-        ),
-      });
+      isCurrentIntervalLong = !isCurrentIntervalLong;
+      clearInterval(counterInterval);
+
+      repeatPromise(alarm, ALARM_REPEAT);
     }
   }, 1000);
 }
@@ -78,3 +125,5 @@ function mainLoop() {
 pauseHandler();
 init();
 drawLabels();
+drawState();
+drawTime();
